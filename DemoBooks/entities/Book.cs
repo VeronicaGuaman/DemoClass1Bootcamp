2b0c9c899b42549c0c4de717ecb62b035ff36c89
Daniel Echeverri <echeverri121@gmail.com>
2023-12-14 17:26:55 -0500

    Book class modified

--- DemoBooks/entities/Book.cs
+++ DemoBooks/entities/Book.cs
@@ -3,9 +3,9 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Category { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Category { get; set; }
         public bool IsAvailable { get; set; }
 
     }
